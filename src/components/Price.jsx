4bab145efa8b0b844e8c2682412209cfece7b3ef
edalet090 <113514842+edalet090@users.noreply.github.com>
2022-11-22 15:57:47 +0400

    Add files via upload

---
+++ src/components/Price.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Price =() => {
+    return (
+        <div>
+       <h2>2000$ </h2>     
+       </div>
+    )
+}
+export default Price
---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Price from './Price'
+import ProductName from './ProductName'
+
+const Product = () => {
+    return(
+        <div>
+            <ProductName />
+            <Price />
+        </div>
+    )
+}
+export default Product
